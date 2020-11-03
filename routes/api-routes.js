0f56c845bf26c9d134a9028d48d2a4c7b646987b
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-11-03 14:07:09 -0600

    added js file for api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,8 @@
+//Create user input on main.handlebars page for "age" , "weight" , "calories (to burn)"
+//Link API images to dataset to prompt a certain image(s) with specific keywords
+
+// using Unsplash for images
+const APIkey = "AAK2cj4m-v8UCbgMQFIoH_WKk_qviQZY-jqypl9CVH0"
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
+const image = 
